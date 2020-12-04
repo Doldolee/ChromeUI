61296aa8c74edfd800a5a21d85f031591f9b967e
y.s.lim <yuseok6853@daum.net>
2020-12-04 15:57:26 +0900

    v1

---
+++ todo.js
@@ -0,0 +1,9 @@
+const todDoForm = document.querySelector("js-toDoForm"),
+toDoInput = form.document.querySelector('input'),
+toDoList = document.querySelector("js-toDoList");
+
+
+function init(){
+    loadToDos();
+}
+init()
